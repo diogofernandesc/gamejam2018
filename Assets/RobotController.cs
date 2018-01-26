a4a0eab02fd7a18ed879088f8427599053346e86
U-SOTON\jc27g15 <jc27g15@JFB59-R3229-B25.soton.ac.uk>
2018-01-26 22:04:39 +0000

    The original and dankest commit

---
+++ Assets/RobotController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Triggered at these brackets
+public class RobotController : MonoBehaviour {
+
+	public Rigidbody2D rb2d;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector2 mov = new Vector2(this.speed * Input.GetAxis ("Horizontal"), this.speed * Input.GetAxis ("Vertical"));
+		this.rb2d.velocity = mov;
+	}
+}
