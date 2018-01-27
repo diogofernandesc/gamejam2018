bf1896655e525d5e5c6f544bf9df14c9fe1dc621
diogofernandesc <diogofernandescon@gmail.com>
2018-01-27 17:15:08 +0000

    added randomising

--- Assets/Scripts/RobotController.cs
+++ Assets/Scripts/RobotController.cs
@@ -7,10 +7,11 @@ public class RobotController : MonoBehaviour {
 
 	public Rigidbody2D rb2d;
 	public float speed;
+	public int health;
 
 	// Use this for initialization
 	void Start () {
-		
+		health = 5;
 	}
 	
 	// Update is called once per frame
@@ -18,4 +19,17 @@ public class RobotController : MonoBehaviour {
 		Vector2 mov = new Vector2(this.speed * Input.GetAxis ("Horizontal"), this.speed * Input.GetAxis ("Vertical"));
 		this.rb2d.velocity = mov;
 	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag ("PickUp")) {
+			health += 1;
+			other.gameObject.SetActive (false);
+		}
+//		} else if (other.gameObject.CompareTag ("Enemy")) {
+//			health -= 1;
+//			Vector2 jumpForce = new Vector2(1,1);
+//			rb2d.AddForce (jumpForce, ForceMode2D.Impulse);
+//		}
+	}
+		
 }
