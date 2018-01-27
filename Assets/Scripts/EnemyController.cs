c337d175c1f6f92acf8a4be35927e309f96dab44
diogofernandesc <diogofernandescon@gmail.com>
2018-01-27 19:50:03 +0000

    flashing wip

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour {
 
 	public Rigidbody2D rb2d;
+	public GameObject player;
+	private float range;
+	public float speed = 2f;
+	private float minDistance = 1f;
 
 	// Use this for initialization
 	void Start () {
@@ -13,15 +17,17 @@ public class EnemyController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		player = GameObject.FindGameObjectWithTag ("Player");
+		range = Vector2.Distance(transform.position, player.transform.position);
+		transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+
 	}
 
 	void OnCollisionEnter2D(Collision2D col) {
+		
 		RobotController rc = col.gameObject.GetComponent<RobotController> ();
 		if (rc != null) {
 			rc.health -= 1;
-
-			Vector2 jumpForce = new Vector2 (-20, -20);
 		}
 	}
 }
